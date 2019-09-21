57c604be6ff734f3c6868edd64a4f8fe9ae21aca
Ваше Имя <you@example.com>
2019-09-21 13:12:24 +0600

    created base and index pages, sorted by date desc

--- source/main/urls.py
+++ source/main/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from webapp.views import *
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', main_page, name='main_url')
 ]
--- source/webapp/views.py
+++ source/webapp/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import Book
 
-# Create your views here.
+
+def main_page(request):
+    if request.method == 'GET':
+        titles = Book.objects.filter(status='active').order_by('-date_create')
+        return render(request, 'index.html', {'titles': titles})
